f7509c7e7ce326f1f204411a5a72e2d84ae68e25
sh41 <sebastian.hartwig@uni-ulm.de>
2015-05-13 12:40:59 +0200

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/sebastianhartwig/com/muc_15_01_14/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sebastianhartwig.com.muc_15_01_14;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "sebastianhartwig.com.lab1";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
